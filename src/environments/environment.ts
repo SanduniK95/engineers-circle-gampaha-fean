74e44f6d9d5aa092dae73e26c4778f531a122fe8
Sanduni Kariyawasam <sandunikariyawasam95@gmail.com>
2019-05-01 10:40:21 +0530

    New components

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBzlekLOg0_Y15S0Ve9tC1FpVKJymDRw8E",
+    authDomain: "engineers-circle-gampaha-fean.firebaseapp.com",
+    databaseURL: "https://engineers-circle-gampaha-fean.firebaseio.com",
+    projectId: "engineers-circle-gampaha-fean",
+    storageBucket: "engineers-circle-gampaha-fean.appspot.com",
+    messagingSenderId: "465713684074"
+  }
 };
