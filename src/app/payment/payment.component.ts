b926bb7b8b824afd2c9481ce14f2bfab9b69dfbc
LahiruDulanjaya <laahirudulanjaya@gmail.com>
2019-06-06 08:32:06 +0530

    payment

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { tap } from 'rxjs/operators';
+import {AuthService} from '../shared/services/auth.service'
 import { Observable } from 'rxjs/Observable';
 import { finalize } from 'rxjs/operators';
 @Component({
@@ -27,7 +30,7 @@ export class PaymentComponent implements OnInit {
     // State for dropzone CSS toggling
     isHovering: boolean;
   
-    constructor(private storage: AngularFireStorage) { }
+    constructor(private storage: AngularFireStorage ,private db: AngularFirestore,private auth :AuthService) { }
   
     
     toggleHover(event: boolean) {
@@ -46,7 +49,7 @@ export class PaymentComponent implements OnInit {
       }
   
       // The storage path
-      const path = `test/${new Date().getTime()}_${file.name}`;
+      const path = `payment/${new Date().getTime()}_${file.name}`;
   
       // Totally optional metadata
       const customMetadata = { app: 'My AngularFire-powered PWA!' };
@@ -56,17 +59,26 @@ export class PaymentComponent implements OnInit {
   
       // Progress monitoring
       this.percentage = this.task.percentageChanges();
-      this.snapshot   = this.task.snapshotChanges()
+      this.snapshot   = this.task.snapshotChanges().pipe(
+        tap(snap => {
+          if (snap.bytesTransferred === snap.totalBytes) {
+            // Update firestore on completion
+            this.db.collection('payment').add( { path, size: snap.totalBytes ,UserId:this.auth.getUserid()})
+          }
+        })
+      )
   
       // The file's download URL
       this.snapshot.pipe(finalize(() => this.downloadURL = this.storage.ref(path).getDownloadURL())).subscribe();
-
     }
   
     // Determines if the upload task is active
     isActive(snapshot) {
       return snapshot.state === 'running' && snapshot.bytesTransferred < snapshot.totalBytes
     }
+
+
+    
 
 }
 
